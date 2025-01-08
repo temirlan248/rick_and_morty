718234c81e182eb26087a2eb4eba32a5c27fe8d8
temirlan_muslimov <temirlan.muslimov2@bcc.kz>
2025-01-08 11:57:52 +0500

    Change name

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp.router(
         routerConfig: _router,
-        title: 'Flutter Demo',
+        title: 'Rick and morty',
         theme: ThemeData(
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
